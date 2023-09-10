3c933bfba70268b0c704eb1226d3ebcf3ceed119
Euris Alexander Aquino <euris051413@gmail.com>
2023-09-10 17:12:34 -0400

    Agregar archivos de proyecto.

---
+++ Connection.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.Runtime.Documents;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThirdParty.Json.LitJson;
+
+namespace Negocio
+{
+    internal class Connection
+    {
+        public void insertData(BsonDocument document) {
+            MongoClient dbClient = new MongoClient("mongodb+srv://EurisAquino:AvjsQVEU2w38B1tY@cluster0.nu42xpp.mongodb.net/");
+
+            var database = dbClient.GetDatabase("Negocio");
+            var collection = database.GetCollection<BsonDocument>("Clientes");
+
+            collection.InsertOne(document);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using ThirdParty.Json.LitJson;
+
+namespace Negocio
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            var connection = new Connection();
+
+            var json = new BsonDocument {
+                { "client_ID", 00002 },
+                { "Nombre","Frandy" },
+                { "Apellidos","Aquino Ulloa" },
+                { "Telefono","8298389495" },
+                { "Sexo","Masculino" },
+                { "Correo","frandi13@gmail.com" }
+                };
+
+            connection.insertData(json);
+            
+        }   
+    }
+}
