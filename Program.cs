44aa4ecaf9b6f48e326ad88fb33f8155be349212
FrandyAquino <82423068+FrandyAquino@users.noreply.github.com>
2023-10-19 17:32:56 -0400

    Se creo el nuevo form de cotizacion

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace Negocio
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SplashScreen());
+            Application.Run(new MainWindow());
             
         }   
     }
---
+++ ui/main_window/Cotizacion_screen/cotizacionForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Negocio.ui.main_window.Cotizacion_screen
+{
+    public partial class cotizacionForm : Form
+    {
+        public cotizacionForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
